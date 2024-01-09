00d0761db3c0f8d81768f61b242a2ed5de503403
GauravDe-git <gm.gauravde@gmail.com>
2024-01-09 20:46:12 +0530

    Player animations

--- Player.h
+++ Player.h
@@ -6,19 +6,50 @@
 #include "SpriteComponent.h"
 #include "Transform2DComponent.h"
 
+class IdleComponent : public Component
+{
+public:
+    SpriteComponent* sprite;
+
+    IdleComponent(const char* filename, int numFrames, float fps = 30.0f)
+        : sprite(new SpriteComponent(filename, numFrames, fps, true)) {}
+};
+
+class RunningComponent : public Component
+{
+public:
+    SpriteComponent* sprite;
+
+    RunningComponent(const char* filename, int numFrames, float fps = 30.0f)
+        : sprite(new SpriteComponent(filename, numFrames, fps, true)) {}
+};
+
+class JumpingComponent : public Component
+{
+public:
+    SpriteComponent* sprite;
+
+    JumpingComponent(const char* filename, int numFrames, float fps = 30.0f)
+        : sprite(new SpriteComponent(filename, numFrames, fps, true)) {}
+};
+
+
 class Player : public Entity
 {
 public:
     bool flip;
     bool jumping;
-    Player(const char* filename, int numFrames, float initialX = 0.0f, float initialY = 0.0f, float fps = 20.0f, bool isAnimated = true)
-	    :flip(false), jumping(false) 
+    Player(const char* idleFilename, const char* runningFilename, const char* jumpingFilename,
+        float initialX = 0.0f, float initialY = 0.0f, float fps = 20.0f, bool isAnimated = true)
+        : flip(false), jumping(false)
     {
         AddComponent(new Transform2DComponent());
         GetComponent<Transform2DComponent>()->position.x = initialX;
         GetComponent<Transform2DComponent>()->position.y = initialY;
 
-        AddComponent(new SpriteComponent(filename, numFrames, fps, isAnimated));
+        AddComponent(new IdleComponent(idleFilename, 26, fps));
+        AddComponent(new RunningComponent(runningFilename, 14, fps));
+        AddComponent(new JumpingComponent(jumpingFilename, 7, 5));
 
         std::vector<Bounds> playerBounds = { Bounds(Tmpl8::vec2(11, 10), Tmpl8::vec2(45, 57)) };
         AddComponent(new ColliderComponent(playerBounds));
@@ -26,4 +57,5 @@ public:
         AddComponent(new PhysicsComponent());
     }
 };
+
 
--- game.cpp
+++ game.cpp
@@ -7,7 +7,7 @@ namespace Tmpl8
 {
 	void Game::Init()
 	{
-		player = new Player("assets/player_idle.png",26,100,100);
+		player = new Player("assets/player_idle.png", "assets/player_run.png", "assets/player_jump.png", 100,100);
 		entities.push_back(player);
 
 		level = new Level("assets/map.png", "assets/foreground.png");
@@ -17,84 +17,99 @@ namespace Tmpl8
 	void Game::Shutdown()
 	{
 	}
-	void Game::Tick(float deltaTime)
-	{
-		//convert dT to seconds
-		deltaTime /= 1000;
+    void Game::Tick(float deltaTime)
+    {
+        //convert dT to seconds
+        deltaTime /= 1000;
 
-		// clear the graphics window
-		screen->Clear(0);
+        // clear the graphics window
+        screen->Clear(0);
 
-		level->Draw(screen);
+        level->Draw(screen);
 
-		// Update and draw all entities
-		for (Entity* entity : entities)
-		{
-			SpriteComponent* sprite = entity->GetComponent<SpriteComponent>();
-			PhysicsComponent* physics = entity->GetComponent<PhysicsComponent>();
-			Transform2DComponent* transform = entity->GetComponent<Transform2DComponent>();
-			ColliderComponent* collider = entity->GetComponent<ColliderComponent>();
+        // Update and draw all entities
+        for (Entity* entity : entities)
+        {
+            PhysicsComponent* physics = entity->GetComponent<PhysicsComponent>();
+            Transform2DComponent* transform = entity->GetComponent<Transform2DComponent>();
+            ColliderComponent* collider = entity->GetComponent<ColliderComponent>();
 
-			if (sprite && transform)
-			{
-				sprite->Update(deltaTime);
-				sprite->Draw(screen, transform->position.x, transform->position.y, 1.0f);
-			}
-			if (physics)
-			{
-				physics->Update(deltaTime);
-				const vec2 oldPosition = transform->position;
+            if (physics && transform)
+            {
+                Player* player = dynamic_cast<Player*>(entity);
+                SpriteComponent* sprite;
+                if (player)
+                {
+                    if (player->jumping)
+                    {
+                        sprite = player->GetComponent<JumpingComponent>()->sprite;
+                    }
+                    else if (physics->velocity.x != 0)
+                    {
+                        sprite = player->GetComponent<RunningComponent>()->sprite;
+                    }
+                    else
+                    {
+                        sprite = player->GetComponent<IdleComponent>()->sprite;
+                    }
+                    sprite->Update(deltaTime);
+                    sprite->Draw(screen, transform->position.x, transform->position.y, 1.0f, player->flip);
+                }
 
-				// Add Gravity 
-				transform->position.y += physics->velocity.y * deltaTime;
-				// Check for collision with the level
-				ColliderComponent* levelCollider = level->GetComponent<ColliderComponent>();
-				if (collider && levelCollider)
-				{
-					for (auto& bound : collider->bounds)
-					{
-						Bounds offsetBounds = bound;
-						offsetBounds.min += transform->position;
-						offsetBounds.max += transform->position;
+                physics->Update(deltaTime);
+                const vec2 oldPosition = transform->position;
 
-						for (auto& levelBound : levelCollider->bounds)
-						{
-							if (Bounds::Collides(offsetBounds, levelBound))
-							{
-								// Collision detected, revert the y position and stop the y velocity
-								transform->position.y = oldPosition.y;
-								physics->velocity.y = 0.0f;
-								player->jumping = false;  // Set jumping to false when a collision with the ground is detected
-								break;
-							}
-						}
-					}
-				}
-				// Add horizontal movement
-				transform->position.x += physics->velocity.x * deltaTime;
-				// Check for collision with the level
-				if (collider && levelCollider)
-				{
-					for (auto& bound : collider->bounds)
-					{
-						Bounds offsetBounds = bound;
-						offsetBounds.min += transform->position;
-						offsetBounds.max += transform->position;
-						for (auto& levelBound : levelCollider->bounds)
-						{
-							if (Bounds::Collides(offsetBounds, levelBound))
-							{
-								// Collision detected, revert the x position
-								transform->position.x = oldPosition.x;
-								break;
-							}
-						}
-					}
-				}
-			}
-			renderSystem.Update(*entity, *screen);
-		}
-	}
+                // Add Gravity 
+                transform->position.y += physics->velocity.y * deltaTime;
+                // Check for collision with the level
+                ColliderComponent* levelCollider = level->GetComponent<ColliderComponent>();
+                if (collider && levelCollider)
+                {
+                    for (auto& bound : collider->bounds)
+                    {
+                        Bounds offsetBounds = bound;
+                        offsetBounds.min += transform->position;
+                        offsetBounds.max += transform->position;
+
+                        for (auto& levelBound : levelCollider->bounds)
+                        {
+                            if (Bounds::Collides(offsetBounds, levelBound))
+                            {
+                                // Collision detected, revert the y position and stop the y velocity
+                                transform->position.y = oldPosition.y;
+                                physics->velocity.y = 0.0f;
+                                player->jumping = false;  // Set jumping to false when a collision with the ground is detected
+                                break;
+                            }
+                        }
+                    }
+                }
+                // Add horizontal movement
+                transform->position.x += physics->velocity.x * deltaTime;
+                // Check for collision with the level
+                if (collider && levelCollider)
+                {
+                    for (auto& bound : collider->bounds)
+                    {
+                        Bounds offsetBounds = bound;
+                        offsetBounds.min += transform->position;
+                        offsetBounds.max += transform->position;
+                        for (auto& levelBound : levelCollider->bounds)
+                        {
+                            if (Bounds::Collides(offsetBounds, levelBound))
+                            {
+                                // Collision detected, revert the x position
+                                transform->position.x = oldPosition.x;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            renderSystem.Update(*entity, *screen);
+        }
+    }
+
 
 	void Game::KeyUp(SDL_Scancode key) 
 	{
